a5b37509eeb99430bda84811b7804b00e744e52b
sailaja-lb <lakkakulasailaja@gmail.com>
2022-06-06 14:18:24 -0500

    starter

---
+++ follower/src/test/java/net/yorksolutions/follower/FollowerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.follower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FollowerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
